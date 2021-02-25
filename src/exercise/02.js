f343c95427e416d19c7329cbd2341136e2fea64a
Joshua Crowe <joshuacrowe@outlook.com>
2021-02-25 22:28:45 +0000

    Exercise 2 Extra Credit 1

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,15 +4,6 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-function Toggle({ children }) {
-  const [on, setOn] = React.useState(false)
-  const toggle = () => setOn(!on)
-
-  return  React.Children.map(children, child => {
-    return React.cloneElement(child, { on, toggle });
-  });
-}
-
 const ToggleOn = ({ children, on }) => on ? children : null;
 
 const ToggleOff = ({ children, on }) => on ? null : children;
@@ -21,13 +12,29 @@ const ToggleButton = ({ on, toggle }) => (
   <Switch on={on} onClick={toggle} />
 )
 
+const JoshToggleButton = ({ on }) => on ? "Oh wow, it's on!" : "Oh dear, it's off!";
+
+const AllowedElementTypes = [ToggleOn, ToggleOff, ToggleButton, JoshToggleButton];
+
+function Toggle({ children }) {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  return  React.Children.map(children, child => {
+    if (!AllowedElementTypes.includes(child.type)) return null;
+    return React.cloneElement(child, { on, toggle });
+  });
+}
+
 function App() {
   return (
     <div>
       <Toggle>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
+        <span>Hello!</span>
         <ToggleButton />
+        <JoshToggleButton />
       </Toggle>
     </div>
   )
